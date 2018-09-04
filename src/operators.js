aa5ce955fb3b3a2e2feb0d6fddeb085de1ddd878
ytknrgn <42815334+ytknrgn@users.noreply.github.com>
2018-09-04 17:07:04 +0100

    operators solutions done

--- src/operators.js
+++ src/operators.js
@@ -1,83 +1,102 @@
 // Rewrite function as arrow function
-function convertToNumber(string){
+const convertToNumber = (string) => +string;
   // function receives a string representation
   // of a number convert it to a number and return it
-}
 
 // Rewrite function as arrow function
-function convertToNegativeNumber(string){
+const convertToNegativeNumber = (string) => -Math.abs(+string);
   // function receives a string representation
   // of a number
 
   // convert string to number and return a negative version of the number
-}
 
 // Rewrite function as arrow function
-function invertBoolean(input){
+const invertBoolean = (input) => (typeof input === "boolean") ? !input : input 
   // function receives one argument which could be of any type
 
   // if it is a boolean negate it, true -> false, false to true
 
   // if the input is not a boolean return the input
-}
 
 // Rewrite function as arrow function
-function convertToBoolean(list){
+const convertToBoolean = (list) => list.map(x => !!x);
   // Function receives an array of values which could be of any type
 
   // Convert all values in array to boolean and return the array
-}
+
 
 // Rewrite function as arrow function
-function deleteStrings(list){
+const deleteStrings = (list) => {
+  for (let i=0;i<list.length;i++) {
+    if (typeof list[i] === "string") {
+      delete list[i];
+    }
+  }
+  return list;
+}
   // Function receives an array of strings and numbers
 
   // delete strings in array, (make them undefined)
 
   // return resulting array
-}
 
 // Rewrite function as arrow function
-function deleteYear(cars){
+const deleteYear = (cars) => {
+  for (let i=0;i<cars.length;i++) {
+    if (cars[i].year < 1995) {
+      delete cars[i].year;
+    }
+  }
+  return cars;
+}
   // cars is an array of objects. Each object has a make, model and year property
 
   // For example { make: 'Ford', model: 'Fiesta', year: 1991 }
 
   // return an array of cars with year property deleted if the year is before 1995
-}
+
 
 // Rewrite function as arrow function
-function isBetween(number, from, to){
+const isBetween = (number, from, to) => number >= from && number < to;
   // function receives three numbers as arguments
 
   // return true if number is equal to or greater than from and less than to
 
   // return false otherwise
-}
 
 // Rewrite function as arrow function
-function evens(a, b, c, d){
+const evens = (a, b, c, d) => (!(a % 2) && !(b % 2) && !(c % 2) && !(d % 2));
   // function receives 4 numbers return true if all numbers are even and false otherwise
 
   // do not use if/else or ternary
-}
+
 
 // Rewrite function as arrow function
-function averageOfStrings(list){
+const averageOfStrings = (list) => {
+  let newlist = list.filter(a => typeof a === "string");
+  return newlist.map(a => +a).reduce((a,b) => (a+b))/newlist.length;}
   // function receives an array of numbers and string representation of numbers
 
   // calculate an average value of numbers represented as strings and return it.
-}
+
 
 // Rewrite function as arrow function
-function footballScores(results){
+const footballScores = (results) => results.map( function(a){
+
+  if (a.home > a.away) {
+    return "home";
+  } else if (a.home < a.away) {
+    return "away";
+  } else {
+    return "draw";
+  }
   // results is an array of football scores represented as objects
 
   // for example: [{home: 0, away: 2}, {home: 3, away: 1}, {home: 1, away: 1}]
 
   // return an array of winning teams or draw if results are same
   // for example: [away, home, draw]
-}
+});
 
 module.exports = {
   convertToNumber,
--- test/operators.test.js
+++ test/operators.test.js
@@ -11,73 +11,73 @@ const {
   footballScores
 } = require('../src/operators');
 
-test('convertToNumber - 1', () => {
+test.only('convertToNumber - 1', () => {
   const result = convertToNumber( '7' );
 
   expect( result ).toEqual( 7 );
 });
 
-test('convertToNumber - 2', () => {
+test.only('convertToNumber - 2', () => {
   const result = convertToNumber( '-11' );
 
   expect( result ).toEqual( -11 );
 });
 
-test('convertToNegativeNumber - 1', () => {
+test.only('convertToNegativeNumber - 1', () => {
   const result = convertToNegativeNumber( '3' );
 
   expect( result ).toEqual( -3 );
 });
 
-test('convertToNegativeNumber - 2', () => {
+test.only('convertToNegativeNumber - 2', () => {
   const result = convertToNegativeNumber( '-15' );
 
   expect( result ).toEqual( -15 );
 });
 
-test('invertBoolean - 1', () => {
+test.only('invertBoolean - 1', () => {
   const result = invertBoolean( true );
 
   expect( result ).toEqual( false );
 });
 
-test('invertBoolean - 2', () => {
+test.only('invertBoolean - 2', () => {
   const result = invertBoolean( false );
 
   expect( result ).toEqual( true );
 });
 
-test('invertBoolean - 3', () => {
+test.only('invertBoolean - 3', () => {
   const result = invertBoolean( 'hello' );
 
   expect( result ).toEqual( 'hello' );
 });
 
-test('convertToBoolean - 1', () => {
+test.only('convertToBoolean - 1', () => {
   const result = convertToBoolean( [null, undefined, 27, 'red', NaN] );
 
   expect( result ).toEqual( [false, false, true, true, false] );
 });
 
-test('convertToBoolean - 2', () => {
+test.only('convertToBoolean - 2', () => {
   const result = convertToBoolean( ['hello', 0, -1, ''] );
 
   expect( result ).toEqual( [true, false, true, false] );
 });
 
-test('deleteStrings - 1', () => {
+test.only('deleteStrings - 1', () => {
   const result = deleteStrings( [11, '12', 0, '0', 35, 'blue'] );
 
   expect( result ).toEqual( [11, undefined, 0, undefined, 35, undefined] );
 });
 
-test('deleteStrings - 2', () => {
+test.only('deleteStrings - 2', () => {
   const result = deleteStrings( [3, 'green', -5, 'orange'] );
 
   expect( result ).toEqual( [3, undefined, -5, undefined] );
 });
 
-test('deleteYear - 1', () => {
+test.only('deleteYear - 1', () => {
   const result = deleteYear( [
     { make: 'Ford', model: 'Fiesta', year: 1991 },
     { make: 'tesla', model: 'Roadster', year: 2020 },
@@ -93,61 +93,61 @@ test('deleteYear - 1', () => {
   ] );
 });
 
-test('isBetween - 1', () => {
+test.only('isBetween - 1', () => {
   const result = isBetween( 27, 20, 40 );
 
   expect( result ).toEqual( true );
 });
 
-test('isBetween - 2', () => {
+test.only('isBetween - 2', () => {
   const result = isBetween( 10, 10, 15 );
 
   expect( result ).toEqual( true );
 });
 
-test('isBetween - 3', () => {
+test.only('isBetween - 3', () => {
   const result = isBetween( 7, 2, 7 );
 
   expect( result ).toEqual( false );
 });
 
-test('isBetween - 4', () => {
+test.only('isBetween - 4', () => {
   const result = isBetween( 35, 10, 20 );
 
   expect( result ).toEqual( false );
 });
 
-test('Evens - 1', () => {
+test.only('Evens - 1', () => {
   const result = evens( 6, 22, 44, 8 );
 
   expect( result ).toEqual( true );
 });
 
-test('Evens - 2', () => {
+test.only('Evens - 2', () => {
   const result = evens( 10, 14, 3, 20 );
 
   expect( result ).toEqual( false );
 });
 
-test('Evens - 3', () => {
+test.only('Evens - 3', () => {
   const result = evens( 3, 7, 15, 21 );
 
   expect( result ).toEqual( false );
 });
 
-test('averageOfStrings - 1', () => {
+test.only('averageOfStrings - 1', () => {
   const result = averageOfStrings( [12, '6', 0, '8', '1'] );
 
   expect( result ).toEqual( 5 );
 });
 
-test('averageOfStrings - 2', () => {
+test.only('averageOfStrings - 2', () => {
   const result = averageOfStrings( ['5', 31, 7, '2', '1', -1, '0'] );
 
   expect( result ).toEqual( 2 );
 });
 
-test('footballScores - 1', () => {
+test.only('footballScores - 1', () => {
   const result = footballScores( [
     {home: 1, away: 2},
     {home: 4, away: 5},
@@ -158,7 +158,7 @@ test('footballScores - 1', () => {
   expect( result ).toEqual( ['away', 'away', 'home', 'draw'] );
 });
 
-test('footballScores - 2', () => {
+test.only('footballScores - 2', () => {
   const result = footballScores( [
     {home: 2, away: 1},
     {home: 4, away: 5},
